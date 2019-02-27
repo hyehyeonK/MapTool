0bb7fe9715ae06d7386533d431150272b1a44265
gudwl <gudwl@DESKTOP-4T047TA>
2019-02-28 01:30:15 +0900

    월드스케일

--- MapTool/Resource.h
+++ MapTool/Resource.h
@@ -47,6 +47,7 @@
 #define IDC_PICTURE                     1030
 #define IDC_EDIT1                       1031
 #define IDC_EDIT2                       1032
+#define IDC_BUTTON2                     1033
 
 // Next default values for new objects
 // 
@@ -54,7 +55,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        318
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           312
 #endif
 #endif
