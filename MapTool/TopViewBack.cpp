5b895b49ef9f6eae937d2026fb063d9151cd5edc
hyehyeonK <kskhh07@gmail.com>
2019-02-27 21:00:36 +0900

    TopView

--- MapTool/TopViewBack.cpp
+++ MapTool/TopViewBack.cpp
@@ -19,8 +19,8 @@ HRESULT CTopViewBack::Initialize()
 	float fX = 0;
 	float fY = 0;
 	//Tile vec
-	//m_fMaxWidth = m_pValueMgr->iRow * m_pValueMgr->iTileW >> 1;
-	//m_fMaxHeight = m_pValueMgr->iColum * m_pValueMgr->iTileH >> 1;
+	m_fMaxWidth = m_pValueMgr->iRow * m_pValueMgr->iTileW;
+	m_fMaxHeight = m_pValueMgr->iColum * m_pValueMgr->iTileH;
 
 	m_vecTile.resize(m_pValueMgr->iColum);
 	for (int iY = 0; iY < m_pValueMgr->iColum; ++iY)
@@ -150,7 +150,7 @@ void CTopViewBack::LineRender()
 	const TEXINFO*		pTexTexture;
 	D3DXVECTOR2 vList[5] = {};
 
-	//float	fX, fY = 0;
+	//float	fX, fY = 0; 
 	m_pGraphicDev->GetLine()->Begin();
 	for (int i = 0; i < m_pValueMgr->iColum; ++i)
 	{
@@ -267,35 +267,23 @@ void CTopViewBack::LineRender()
 
 POINT CTopViewBack::GetTileIdx(const D3DXVECTOR3 & _vPos)
 {
+
+
+
 	float fSlop = float(TILECY) / TILECX;
 	float fY = 0;
 	float fX = 0;
-	int		m_iTargetIdxY = 0;
-	int		m_iTargetIdxX = 0;
+	int		iColum = 0;
+	int		iRow = 0;
 
-	float fGradient = g_MGR_VALUE->iTileH / float(g_MGR_VALUE->iTileW);
 
-	m_iTargetIdxY = g_MGR_VALUE->iColum - 1;
-	m_iTargetIdxX = g_MGR_VALUE->iRow - 1;
+	
 
-	for (; m_iTargetIdxX >= 0; --m_iTargetIdxX)
-	{
-		if (((_vPos.x - (m_vecTile[0][m_iTargetIdxX]->vPos.x - (g_MGR_VALUE->iTileW >> 1))) * -fGradient) <= _vPos.y - m_vecTile[0][m_iTargetIdxX]->vPos.y)
-			break;
-	}
+	iRow = _vPos.x / g_MGR_VALUE->iTileW;
+	iColum = _vPos.y / g_MGR_VALUE->iTileH;
 
-	if (m_iTargetIdxX < 0)
+	if (iColum  < 0 || iColum >= g_MGR_VALUE->iColum || iRow >= g_MGR_VALUE->iRow || iRow < 0)
 		return{ -1,-1 };
 
-	for (; m_iTargetIdxY >= 0; --m_iTargetIdxY)
-	{
-		if (((_vPos.x - (m_vecTile[m_iTargetIdxY][0]->vPos.x + (g_MGR_VALUE->iTileW >> 1))) * fGradient) <= _vPos.y - m_vecTile[m_iTargetIdxY][0]->vPos.y)
-			break;
-	}
-
-	if (m_iTargetIdxY < 0)
-		return{ -1,-1 };
-
-	//return{ m_iTargetIdxY,m_iTargetIdxX };
-	return{ m_iTargetIdxX, m_iTargetIdxY };
+	return{ iColum, iRow };
 }
