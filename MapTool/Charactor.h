225485aa4123802e11afa8b763f1a2aec4f00691
hyehyeonK <kskhh07@gmail.com>
2019-02-27 21:57:12 +0900

    ee

--- MapTool/Charactor.h
+++ MapTool/Charactor.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "Obj.h"
-class CCharactor :
-	public CObj
+class CCharactor : public CObj
 {
 public:
 	CCharactor();
 	~CCharactor();
+
+public:
+	virtual HRESULT Initialize() override;
+	virtual int Progress() override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+
 };
 
