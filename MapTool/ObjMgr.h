a88256df847dfc69303f7ccd298b44523f2861ee
Yun <Yun@DESKTOP-QTK7QOK>
2019-02-27 23:40:49 +0900

    f

--- MapTool/ObjMgr.h
+++ MapTool/ObjMgr.h
@@ -1,16 +1,30 @@
 #pragma once
 #include "../Headers/Include.h"
 
+#define g_MGR_OBJ CObjMgr::GetInstance()
+
 class CObjMgr
 {
 	DECLARE_SINGLETON(CObjMgr)
+
+public:
+	enum OBJID
+	{
+
+		OBJ_END
+	};
 
 private:
 	CObjMgr();
 	~CObjMgr();
 
+public:
+	//vector<INFO*> m_vecObj&	GetObjects() { return m_vecObj; }
+	//void InitTiles();
+	//void Release();
+
 private:
-	//vector<> m_vecTile;
+	vector<INFO*> m_vecObj[OBJ_END];
 
 };
 
